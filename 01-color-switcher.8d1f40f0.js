7bef9dcf2f59273e111cfec148927148d236bf58
SvetaSaletska <112178891+SvetaSaletska@users.noreply.github.com>
2023-10-31 18:10:09 +0000

    Deploying to gh-pages from @ SvetaSaletska/goit-js-hw-09@ac4f2bbe0b2954624593eb157152f48abf737266 🚀

---
+++ 01-color-switcher.8d1f40f0.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`})),console.log(t.startBtn);
+//# sourceMappingURL=01-color-switcher.8d1f40f0.js.map
