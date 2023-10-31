444349d1afdabcec5e64f5f437d495ea771246f2
SvetaSaletska <svitlanagalevich@gmail.com>
2023-11-01 01:06:41 +0200

    update - 02

---
+++ src/js/02-timer.js
@@ -0,0 +1,30 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+
+const selectors = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+}
+
+let days = document.querySelector('span[data-days]');
+let hours = document.querySelector('span[data-hours]');
+let minutes = document.querySelector('span[data-minutes]');
+let seconds = document.querySelector('span[data-seconds]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const currentDate = new Date();
+
+console.log(currentDate)
+
+const flatpickr =  flatpickr(input, {options});
