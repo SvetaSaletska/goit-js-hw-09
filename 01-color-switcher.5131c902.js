f1a1a3fb8ef13962bf8e9a64bc82ed3cf4fab656
SvetaSaletska <112178891+SvetaSaletska@users.noreply.github.com>
2023-10-31 18:41:11 +0000

    Deploying to gh-pages from @ SvetaSaletska/goit-js-hw-09@0f1d96f4ce876d12a757670f99f18c7717169881 🚀

---
+++ 01-color-switcher.5131c902.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),o=(document.querySelector("[data-stop]"),document.querySelector("body"));t.addEventListener("click",(()=>{const t=setTimeout((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;o.style.backgroundColor=t,color.textContent=t}),1e3);console.log(t)}));
+//# sourceMappingURL=01-color-switcher.5131c902.js.map
--- 01-color-switcher.806b8dcf.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))})),console.log(t.startBtn)}();
-//# sourceMappingURL=01-color-switcher.806b8dcf.js.map
