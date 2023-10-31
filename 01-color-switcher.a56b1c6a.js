cfb7a9deb2a279f010cc0d7143b1184124650780
SvetaSaletska <112178891+SvetaSaletska@users.noreply.github.com>
2023-10-31 22:26:57 +0000

    Deploying to gh-pages from @ SvetaSaletska/goit-js-hw-09@47a380beec73437b75acc5b45bc7053d47835d11 🚀

---
+++ 01-color-switcher.a56b1c6a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let a=null;e.addEventListener("click",(l=>{e.disabled=!0,t.disabled=!1,a=setInterval((()=>{let e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.style.backgroundColor=e}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.a56b1c6a.js.map
--- 01-color-switcher.f0d4f7be.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),o=(document.querySelector("[data-stop]"),document.querySelector("body"));t.addEventListener("click",(function(){var t=setTimeout((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));o.style.backgroundColor=t,color.textContent=t}),1e3);console.log(t)}))}();
-//# sourceMappingURL=01-color-switcher.f0d4f7be.js.map
