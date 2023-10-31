0f1d96f4ce876d12a757670f99f18c7717169881
SvetaSaletska <svitlanagalevich@gmail.com>
2023-10-31 20:40:46 +0200

    update 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,30 @@
-const elements = {
-  startBtn: document.querySelector('[data-start]'),
-  stopBtn: document.querySelector('[data-stop]'),
-  body: document.querySelector('body')
-}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body')
 
-elements.startBtn.addEventListener('click', getRandomHexColor);
+
+
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-console.log(elements.startBtn)
 
+  const onClick = () => {
+    const timerId = setTimeout(() => {
+      let randClr = getRandomHexColor();
+      body.style.backgroundColor = randClr;
+      color.textContent = randClr;
+    }, 1000);
+
+    console.log(timerId);
+  };
+
+  startBtn.addEventListener('click', onClick);
+
+
+  // function handlerClick(evt) {
+  //   let randClr = getRandomHexColor();
+  //     body.style.backgroundColor = randClr;
+  //     color.textContent = randClr;
+  //   }
