6be2884ebf1d824be03de06b25b09a8f59e9cb7b
SvetaSaletska <svitlanagalevich@gmail.com>
2023-11-01 14:52:44 +0200

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,18 +13,50 @@ let hours = document.querySelector('span[data-hours]');
 let minutes = document.querySelector('span[data-minutes]');
 let seconds = document.querySelector('span[data-seconds]');
 
+const currentDate = new Date();
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] <= new Date()) {
+    alert('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+      let ms = selectedDates[0].getTime() - currentDate.getTime();
+    }
+
+    setInterval(() => {
+      startBtn.addEventListener('click', convertMs);
+      function convertMs(ms) {
+
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+
+        const days = Math.floor(ms / day);
+        const hours = Math.floor((ms % day) / hour);
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+        return { days, hours, minutes, seconds };
+      }
+
+      days.textContent = convertMs(ms).days;
+      hours.textContent = convertMs(ms).hours;
+      minutes.textContent = convertMs(ms).minutes;
+      seconds.textContent = convertMs(ms).seconds;
+
+      ms -=1;
+
+    })
   },
 };
 
-const currentDate = new Date();
+
 
 console.log(currentDate)
-
-const flatpickr =  flatpickr(input, {options})
