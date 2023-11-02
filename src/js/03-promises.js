1575a8ba32a6d84b14d051955824f4e72deab361
SvetaSaletska <svitlanagalevich@gmail.com>
2023-11-02 16:56:35 +0200

    task-03-done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,27 +1,28 @@
 import Notiflix from 'notiflix';
 
-const elements = {
-form: document.querySelector('.form'),
-delay: document.querySelector('input[name="delay"]'),
-step: document.querySelector('input[name="step"]'),
-amount: document.querySelector('input[name="amount"]'),
-};
+const form = document.querySelector('.form');
+const inputDelay = form.elements.delay;
+const inputStep = form.elements.step;
+const inputAmount = form.elements.amount;;
+const btnCreatePromise = document.querySelector('button');
+
+
+btnCreatePromise.addEventListener('click', submit);
 
 function submit(evt) {
   evt.preventDefault();
-  let delay = Number(elements.delay.value);
-  const step = Number(elements.step.value);
-  const amount = Number(elements.amount.value);
-  let position;
-  for (let i = 1; i <= amount; i += 1){
-    createPromise(i, elements.delay);
-    delay += elements.step;
+  let delay = Number(inputDelay.value);
+  const step = Number(inputStep.value);
+  const amount = Number(inputAmount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay);
+    delay += step;
   }
-}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  const promise = new Promise ((resolve, reject) => {
+  let promise = new Promise ((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
         resolve({position, delay});
@@ -29,17 +30,15 @@ function createPromise(position, delay) {
         reject({position, delay});
       }
     }, delay)
-  })
+});
 
   promise
-
-  .then(({position, delay}) => {
+    .then(({position, delay}) => {
     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
-  .catch(({ position, delay }) => {
+    .catch(({ position, delay }) => {
     Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   });
   }
+}
 
